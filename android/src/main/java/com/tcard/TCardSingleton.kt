79d266a441fb1a33a561ce45426d094316a4559b
Azshar <marikyan1993@gmail.com>
2025-01-22 22:31:21 +0300

    - add singleton

---
+++ android/src/main/java/com/tcard/TCardSingleton.kt
@@ -0,0 +1,19 @@
+package com.tcard
+
+import androidx.activity.ComponentActivity
+import ru.tbank.posterminal.p2psdk.TSoftposManager
+
+class TCardSingleton () {
+    companion object {
+        @Volatile private var INSTANCE: TSoftposManager? = null
+
+        fun getInstance(activity: ComponentActivity) =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: TSoftposManager(activity).also { INSTANCE = it }
+            }
+
+        fun getInstance(): TSoftposManager? {
+            return INSTANCE;
+        }
+    }
+}
