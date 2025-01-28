aa195e80aa29821a8e0b6abeaf9ad7d77c8ef89f
Azshar <marikyan1993@gmail.com>
2025-01-28 22:49:23 +0300

    feat: - back ios folder

---
+++ ios/Tcard.swift
@@ -0,0 +1,7 @@
+@objc(Tcard)
+class Tcard: NSObject {
+  @objc(multiply:withB:withResolver:withRejecter:)
+  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+    resolve(a*b)
+  }
+}
